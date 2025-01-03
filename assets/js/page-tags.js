1915806bc17c62de34d4b20ee8ad83988986a7f9
Hogne Titlestad <ht@friendup.cloud>
2025-01-03 11:31:48 +0100

    Support selectors in various languages.

--- assets/js/page-tags.js
+++ assets/js/page-tags.js
@@ -5,7 +5,7 @@ window.toolbar.tags = function() {
     let tct = document.getElementById( 'top_chat_title' );
     let tcd = tct.getElementsByTagName( 'div' )[0];
     let tci = tct.getElementsByTagName( 'div' )[1];
-    tcd.innerHTML = 'Tags';
+    tcd.innerHTML = 'Tags: ' + currentEditor.filename;
     tcd.className = 'tags';
     tci.innerHTML = ''; //<em class="chat-reset" title="Reset chat"></em><em class="chat-save" title="Save chat"></em><em class="chat-more" title="Options"></em>';
     
