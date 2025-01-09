ed02752508e5abee39a89ee53dbbeaf57ef00fde
Hogne Titlestad <ht@friendup.cloud>
2025-01-09 21:43:19 +0100

    Signal engine implemented.

--- assets/js/signals.js
+++ assets/js/signals.js
@@ -1,1 +1,33 @@
 // Sends signals with callbacks using our microserver
+window.callbackIds = 0;
+window.sendSignal = function( signal, callback )
+{
+    let callbackId = window.callbackIds++;
+    window.callbacks[ callbackId ] = function( data )
+    {
+        if( callback ) callback( base64DecodeUtf8( data ) );
+    }
+    window.webkit.messageHandlers.receiveSignal.postMessage( callbackId + "\n" + signal );
+}
+window.callbacks = {};
+window.executeSignalCallback = function( callbackId, data = '' )
+{
+    // Execute and clean up
+    if( window.callbacks[ callbackId ] )
+    {
+        window.callbacks[ callbackId ]( data );
+        let o = {};
+        for( let a in window.callbacks )
+        {
+            if( a != callbackId )
+            {
+                out[ a ] = window.callbacks[ a ];
+            }
+        }
+        window.callbacks = o;
+    }
+}
+window.addEventListener( 'message', function( data )
+{
+    console.log( '[signal] Got message: ', data );
+} );
